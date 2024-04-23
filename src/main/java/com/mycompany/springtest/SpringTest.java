75284e50a331d45fc84d62196ffc6b47f0050ec4
KaliyevBekzhan <144799131+KaliyevBekzhan@users.noreply.github.com>
2024-04-23 15:51:00 +0500

    Создал пустой репозиторий, настроил для работы с Spring-Boot

---
+++ src/main/java/com/mycompany/springtest/SpringTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.springtest;
+
+/**
+ *
+ * @author rekal
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTest.class, args);
+    }
+}
---
+++ src/main/java/com/mycompany/springtest/controllers/MyController.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/springframework/Controller.java to edit this template
+ */
+package com.mycompany.springtest.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author rekal
+ */
+@Controller
+public class MyController {
+    
+    @RequestMapping("/hello")
+    public String page() {
+        return "hello";
+    }
+    
+}
